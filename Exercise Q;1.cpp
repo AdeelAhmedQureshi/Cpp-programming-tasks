36d5cf42d3458b7f23c7554872b58215e2dfb0e0
Adeel Ahmed Qureshi <118745628+AdeelAhmedQureshi@users.noreply.github.com>
2024-07-18 12:00:14 +0500

    Add files via upload

---
+++ Exercise Q;1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+	int a,b;
+    float c;
+    cout<<"Enter integer  value: ";   cin>>a;
+    cout<<"Enter integer  value: ";cin>>b;
+    cout<<"Enter float value:    ";   cin>>c;
+    cout<<endl<<endl;
+   
+    cout<<"integer1: "<<a<<endl;
+	cout<<"integer2: "<<b<<endl;
+	cout<<"float1:   "<<c<<endl;
+
+	system("PAUSE") ;
+	return 0 ;
+}
+	
+
---
+++ Q;02 my name.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+	// ( ADEEl ) writtren with stars ( * ) .
+	//letter A.
+	cout<<"\n\t    * \n";cout<<"\t   *  *\n";cout<<"\t  * ** *\n";cout<<"\t *      *\n";cout<<"\t*        *\n\n\n";
+	//letter D.
+	cout<<"\t******* \n";cout<<"\t*      *\n";cout<<"\t*      *\n";cout<<"\t*      *\n";cout<<"\t*      *\n";cout<<"\t*      *\n";cout<<"\t******* \n\n\n";
+	//letter E , same letter repeat so i copy its coding.
+	cout<<"\t********\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t********\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t********\n\n\n";
+	//letter E agian.
+	cout<<"\t********\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t********\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t********\n\n\n";
+	//Now last letter L.
+	cout<<"\t*\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t*\n";cout<<"\t********\n\n\n\n";
+	
+	
+	
+	
+	
+	system("PAUSE") ;
+	return 0 ;
+}
+	
+
